4bd45254b3a5315d7b228f324a71f0cfce922a37
Jirachai Chansivanon <antronic.inc@gmail.com>
2018-04-23 19:22:00 +0700

    [ADD] loop tutorial

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Jirachai Chansivanon' });
 });
+
+router.get('/my-nick', function(req, res) {
+  res.render('nick', { mynickname: 'Job Za 555+' })
+})
+
+router.get('/users', function(req, res) {
+  const users = [
+    {
+      name: "Jim",
+      age: 12,
+    },
+    {
+      name: "Tom",
+      age: 21,
+    },
+    {
+      name: "John",
+      age: 23,
+    },
+  ]
+  // { users }
+  // { users: users }
+  res.render('user/show-all', { users })
+})
 
 module.exports = router;
